5fbe3f68557e473c7296165ddc2f9d4d3084f4e5
Massa7ca <32760051+Massa7ca@users.noreply.github.com>
2022-09-26 18:55:16 +0600

    Update 76.py

--- 76.py
+++ 76.py
@@ -1,2 +1,9 @@
-#!/usr/bin/python
-import math,time
+def _76_(n):
+    history = [1]+[0]*n
+    for t in range(1, n+1):
+        for i, x in enumerate(range(t, n+1)):
+            history[x] += history[i]
+    return history[-1]
+
+
+print(_76_(100)-1)
