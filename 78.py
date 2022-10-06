e41fd3e3d0568eb832b7b2e800dffad3863c11ae
Massa7ca <32760051+Massa7ca@users.noreply.github.com>
2022-10-07 05:08:22 +0600

    Update 78.py

--- 78.py
+++ 78.py
@@ -2,9 +2,11 @@ def _78_(n, dil):
     history = [1] + [0] * n
     for t in range(1, n + 1):
         for i, x in enumerate(range(t, n + 1)):
-            history[x] += (history[i] % dil)
+            history[x] += history[i]
         if history[t] % dil == 0:
             return t
+        if t % 100 == 0:
+            print(t)
 
 print(_78_(100000, 1000000))
 # 11224 - 100000
